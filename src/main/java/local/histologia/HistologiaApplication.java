b6c14df66ade96833eedcc0a5d2873fc13f044c8
Josius <josius.amaro@gmail.com>
2023-07-26 00:10:43 -0300

    config base do projeto - zoomify funcionando

---
+++ src/main/java/local/histologia/HistologiaApplication.java
@@ -0,0 +1,13 @@
+package local.histologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/histologia/HistologiaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package local.histologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistologiaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
