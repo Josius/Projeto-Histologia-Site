af290da8fb8221fdea9bd5bd51a0f3ae0651cfee
Josius <josius.amaro@gmail.com>
2023-07-28 14:35:47 -0300

    refactor(VisualizaçãoController): utilizando um único objeto para renderizar o html
    - adicionado um arrayList para receber os parâmetros necessários para renderizar a página de visualização zoomable e enviando-o p/ a camada View
    - reconfigurando lamina.html p/receber o objeto do back-end
    - padronização dos arquivos .htm

--- src/main/java/local/histologia/controllers/LaminaController.java
+++ src/main/java/local/histologia/controllers/LaminaController.java
@@ -3,6 +3,8 @@ package local.histologia.controllers;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +18,36 @@ public class LaminaController {
 
 		ModelAndView mv = new ModelAndView("visualizando/lamina.html");
 
+		List<String> objeto = new ArrayList<>();
+		objeto.add("Pele Fina");
+		// objeto.add("Pele Grossa");
+		
+		
 		String l = "";
 		try (BufferedReader inHtml = new BufferedReader(
-				// new FileReader("src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm"))) {
-					
-				new FileReader("src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm"))) {
-			StringBuilder sb = new StringBuilder();
-
-			while ((l = inHtml.readLine()) != null) {
-				sb.append(l);
+			new FileReader("src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm"))) {
+			// new FileReader("src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm"))) {
+				StringBuilder sb = new StringBuilder();
+				
+				while ((l = inHtml.readLine()) != null) {
+					sb.append(l);
+				}
+				l = sb.toString();
+				objeto.add(l);
+			} catch (Exception e) {
+				System.out.println("ERRRROUUROUROURO");
+				e.printStackTrace();
 			}
-			l = sb.toString();
-			mv.addObject("inHtml", l);
-		} catch (Exception e) {
-			System.out.println("ERRRROUUROUROURO");
-			e.printStackTrace();
-		}
+			objeto.add("Images/L60PelefinaHE40XB.pff");
+			// objeto.add("Images/L01PeleGrossaCoximdeGatoL2HE40X.pff");
+			
+			
+			objeto.add("zAnnotationPath=Assets/Annotations/Narratives/L60PelefinaHE40XB-annotations.xml");
+			// objeto.add("zAnnotationPath=Assets/Annotations/Narratives/L01PeleGrossaCoximdeGatoL2HE40X-annotations.xml");
+			
 
-		return mv;
+			mv.addObject("configs", objeto);
+			
+			return mv;
 	}
 }
