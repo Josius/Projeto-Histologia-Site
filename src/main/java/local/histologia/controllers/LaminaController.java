bab5c37fbdeb0e27342a23969add9687361b4b56
Josius <josius.amaro@gmail.com>
2023-08-01 21:42:48 -0300

    refactor(VisualizaçãoController): reformulando backend e separando páginas html e arquivos css para melhor manutenção
            - retirada de imports e código que não estavam sendo utilizado: camada Controller e Service
            - exclusão do arquivo layoutVisualização.html e centralizando visualização em lamina.html
            SEPARAÇÃO DE CÓDIGO PARA MELHOR MANUTENÇÃO:
            - retirada de códigos inúteis e comentários em lamina.html
            - implantação de fragments em lamina.html: header e article
            - exclusão do arquivo estilo.css
            - desmembramento do arquivo estilo.css em 3 outros arquivos no diretório /css/visualizando/
            - criação dos arquivos geral.css, article.css e header.css

--- src/main/java/local/histologia/controllers/LaminaController.java
+++ src/main/java/local/histologia/controllers/LaminaController.java
@@ -1,15 +1,8 @@
 package local.histologia.controllers;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import local.histologia.entities.Lamina;
@@ -27,51 +20,9 @@ public class LaminaController {
 		ModelAndView mv = new ModelAndView("visualizando/lamina.html");
 
 		Lamina lamina = laminaService.getLamina(2L);
-		// Lamina lamina = laminaService.criaLamina();
 		
 		mv.addObject("lamina", lamina);
 
 		return mv;
 	}
-
-	// @RequestMapping("lamina")
-	// public ModelAndView lamina() throws IOException {
-
-	// ModelAndView mv = new ModelAndView("visualizando/lamina.html");
-
-	// List<String> objeto = new ArrayList<>();
-	// objeto.add("Pele Fina");
-	// // objeto.add("Pele Grossa");
-
-	// String l = "";
-	// try (BufferedReader inHtml = new BufferedReader(
-	// new
-	// FileReader("src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm")))
-	// {
-	// // new
-	// FileReader("src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm")))
-	// {
-	// StringBuilder sb = new StringBuilder();
-
-	// while ((l = inHtml.readLine()) != null) {
-	// sb.append(l);
-	// }
-
-	// l = sb.toString();
-	// objeto.add(l);
-	// } catch (Exception e) {
-	// System.out.println("ERRRROUUROUROURO");
-	// e.printStackTrace();
-	// }
-	// objeto.add("Images/L60PelefinaHE40XB.pff");
-	// // objeto.add("Images/L01PeleGrossaCoximdeGatoL2HE40X.pff");
-
-	// objeto.add("zAnnotationPath=Assets/Annotations/Narratives/L60PelefinaHE40XB-annotations.xml");
-	// //
-	// objeto.add("zAnnotationPath=Assets/Annotations/Narratives/L01PeleGrossaCoximdeGatoL2HE40X-annotations.xml");
-
-	// mv.addObject("configs", objeto);
-
-	// return mv;
-	// }
 }
--- src/main/java/local/histologia/services/LaminaService.java
+++ src/main/java/local/histologia/services/LaminaService.java
@@ -34,36 +34,11 @@ public class LaminaService {
 		lamina.get().setDescricaoHtm(getDescricaoHtm(lamina.get().getDescricaoHtm()));
 	}
 
-	public Lamina criaLamina() {
-		Lamina laminaPFina = new Lamina();
-		laminaPFina.setNome("Pele Fina");
-		laminaPFina.setDescricaoHtm(
-				getDescricaoHtm("src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm"));
-		laminaPFina.setRotaPff("Images/L60PelefinaHE40XB.pff");
-		laminaPFina.setRotaXml("zAnnotationPath=Assets/Annotations/Narratives/L60PelefinaHE40XB-annotations.xml");
-
-		Lamina laminaPGrossa = new Lamina();
-		laminaPGrossa.setNome("Pele Grossa");
-		laminaPGrossa.setDescricaoHtm(
-				getDescricaoHtm("src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm"));
-		laminaPGrossa.setRotaPff("Images/L01PeleGrossaCoximdeGatoL2HE40X.pff");
-		laminaPGrossa.setRotaXml(
-				"zAnnotationPath=Assets/Annotations/Narratives/L01PeleGrossaCoximdeGatoL2HE40X-annotations.xml");
-
-		return laminaPFina;
-		// return laminaPGrossa;
-	}
-
 	private String getDescricaoHtm(String rotaHtm) {
 		String descricao = "";
 		try (BufferedReader inHtml = new BufferedReader(
 				new FileReader(rotaHtm))) {
-			// new
-			// FileReader("src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm")))
-			// {
-			// new
-			// FileReader("src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm")))
-			// {
+			
 			StringBuilder sb = new StringBuilder();
 
 			while ((descricao = inHtml.readLine()) != null) {
