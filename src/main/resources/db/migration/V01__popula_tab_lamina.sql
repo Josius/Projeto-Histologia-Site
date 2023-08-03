1f71bf1b194c9554a639f032216f40455deb378d
Josius <josius.amaro@gmail.com>
2023-08-03 19:57:06 -0300

    build(flyway_postgresql): adiciona e configura flyway para versionamento do banco de dados
    - configuração do pom.xml: implantação da dependência flyway
    - configuração do application.properties: acesso ao BD, mas propriedade de autocriar tabelas comentada
    - criação de dois scripts:
        - V00__criacao_tab_lamina.sql: cria a tabela lamina para alocar dados de cada lâmina
        - V01__popula_tab_lamina.sql: popula a tabela lamina com dados de duas lâminas

---
+++ src/main/resources/db/migration/V01__popula_tab_lamina.sql
@@ -0,0 +1,20 @@
+INSERT INTO lamina(
+	id,
+	nome,
+	descricao_htm,
+	rota_pff,
+	rota_xml
+) VALUES (
+	'f391e054-6dde-4198-bdbd-14fa9496e2c4',
+	'Pele Fina',
+	'src/main/resources/templates/Assets/Narratives/L60PelefinaHE40XB.htm',
+	'Images/L60PelefinaHE40XB.pff',
+	'zAnnotationPath=Assets/Annotations/Narratives/L60PelefinaHE40XB-annotations.xml'
+),
+(
+	'ac9344eb-2f29-4402-bba9-82a353b8899e',
+	'Pele Grossa',
+	'src/main/resources/templates/Assets/Narratives/L01PeleGrossaCoximdeGatoL2HE40.htm',
+	'Images/L01PeleGrossaCoximdeGatoL2HE40X.pff',
+	'zAnnotationPath=Assets/Annotations/Narratives/L01PeleGrossaCoximdeGatoL2HE40X-annotations.xml'
+);
